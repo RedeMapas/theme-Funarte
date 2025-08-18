65c6315b0a603bb68517522a92e2ea0213adbec3
Lucas Pirola <lucaspirola@gmail.com>
2025-08-18 20:07:47 -0300

    Atualiza filtro de agentes para ser mais reistrito e evitar bug no meu perfil

--- Theme.php
+++ Theme.php
@@ -22,9 +22,19 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme
             $this->part('google-analytics--script');
             $this->part('clarity--script');
         });
-        $app->hook("ApiQuery(<<project|opportunity|event|space|agent|seal>>).params", function(&$api_params) use($app) {
+
+        $app->hook("ApiQuery(<<project|opportunity|event|space|seal>>).params", function(&$api_params) use($app) {
             if($subsite = $app->subsite){
                 $api_params['_subsiteId'] = API::EQ($subsite->id);
+            }
+        });
+
+        $app->hook("ApiQuery(agent).params", function(&$api_params) use($app) {
+            $path = $app->request->getPathInfo();
+            if(strpos($path, '/agentes') !== false || strpos($path, '/agents') !== false) {
+                if($subsite = $app->subsite){
+                    $api_params['_subsiteId'] = API::EQ($subsite->id);
+                }
             }
         });
 
